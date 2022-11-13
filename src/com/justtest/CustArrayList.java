98d7ad54151067dd7d09b4c3812bcb31202b6fa9
Ramakant Kumar <ramakantnmiet123@gmail.com>
2022-11-13 17:01:50 +0530

    Customize ArrayList Completed

--- src/com/justtest/CustArrayList.java
+++ src/com/justtest/CustArrayList.java
@@ -1,5 +1,25 @@
 package com.justtest;
 
-public class CustArrayList {
+import java.beans.Customizer;
+import java.util.ArrayList;
 
+public class CustArrayList extends ArrayList<Object> {
+	public boolean add(Object o) {
+		if(this.contains(o)) {
+			return true;
+		}else {
+			return super.add(o);
+		}
+	}
+	public static void main(String[] args) {
+		CustArrayList ca=new CustArrayList();
+		ca.add(1);
+		ca.add("india");
+		ca.add(1);
+		ca.add("india");
+		ca.add(1);
+		ca.add("india");
+		System.out.println(ca);
+		
+	}
 }
